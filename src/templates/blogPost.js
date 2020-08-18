95a6f51440b0127a2d66a4ed91a40af7df4b561b
Kenneth Lyerly <kenput3r@gmail.com>
2020-08-18 15:28:10 -0700

    add slug to location prop

--- src/templates/blogPost.js
+++ src/templates/blogPost.js
@@ -46,7 +46,7 @@ export default function Template({
   const { markdownRemark } = data // data.markdownRemark holds your post data
   const { frontmatter, html } = markdownRemark
   return (
-    <Layout location="/blog">
+    <Layout location={frontmatter.slug}>
       <SEO title={frontmatter.title + " - " + frontmatter.date} />
       <PageHeader />
       <Container>
